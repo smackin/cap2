822e08c50775b6c55fea6c2a0825882eaee68554
Stephanie <88047028+smackin@users.noreply.github.com>
2023-03-26 10:59:32 -0400

    Add files via upload

--- index.js
+++ index.js
@@ -1,20 +1,50 @@
 const express = require('express');
 const app = express();
 const db = require('./db');
+// const cors = require('cors');
 
 app.use(express.json());
 
-app.get('/getUsers', async (req, res) => {
-    try  {
-	const res = await db.query(`SELECT * FROM fyfreactapp_users`);
+// app.use(cors());
 
-	return res.json(results.rows);
-} catch (error) {
-    console.log(error.message)
-    return next(error); 
-}
-} );
+app.get('/user', async (req, res, next) => {
+    try {
+        const result = await db.query(`SELECT * FROM fyfreactapp_users`);
+        return res.status(200).json({ allUsers:result.rows});
+    } catch (err) {
+        console.log(err)
+    }
+    console.log(res.body)
+    
+})
+
+app.get('/user/:user_id', async (req, res, next) => {
+    try {
+        console.log(req.params)
+        const user_id = req.params.user_id;
+        const result = await db.query(`SELECT * FROM fyfreactapp_users WHERE user_id = ${user_id}`);
+        return res.status(200).json({ allUsers:result.rows});
+    } catch (err) {
+        console.log(err)
+    }
+    console.log(res.body)
+    
+})
+
+
+    //     try  {
+// 	res.send = await db.query(`SELECT * FROM fyfreactapp_users`);
+
+// 	console.log(res.body)
+// } catch (error) {
+//     console.log(error.message)
+//     return next(error); 
+// }
+// console.log(res.body)
+// } );
 
 app.listen(5000, function () {
     console.log('App on port 5000');
 });
+
+
